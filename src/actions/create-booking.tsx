7f9447cb1ce06d22ff2f014a493103cd17bc86da
Helton Batista <=>
2025-06-27 01:11:50 -0300

    fix: secure createBooking action with server sessionorganization setup

--- src/actions/create-booking.tsx
+++ src/actions/create-booking.tsx
@@ -1,26 +1,35 @@
 "use server";
 
 import { db } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface createBookingParams {
   serviceId: string;
-  userId: string;
   date: Date;
   barbershopId?: string;
 }
-
 export const createBooking = async ({
-  userId,
   serviceId,
   date,
   barbershopId,
 }: createBookingParams) => {
-  await db.booking.create({
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.id) {
+    throw new Error("Acesso negado. Usuário não autenticado.");
+  }
+
+  const userIdFromSession = session.user.id;
+
+  const booking = await db.booking.create({
     data: {
-      userId,
+      userId: userIdFromSession,
       serviceId,
       date,
       barbershopId,
     },
   });
+
+  return booking;
 };
