982ca3a15cc1f72657c11c63c828bbbdec455576
Helton Batista <=>
2025-06-29 13:41:14 -0300

    fix: import of the hearder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "@/components/headers";
 import Image from "next/image";
 import { db } from "@/lib/prisma";
 import BarbershopItem from "@/components/barbershop-item";
